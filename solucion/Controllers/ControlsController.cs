603b932b15a0e54a8abe0bacdc4df9dccd9f159d
CristhianDavid <emmacris04@gmail.com>
2024-04-17 21:34:13 -0500

    Agregando vista de la lista controlers

--- solucion/Controllers/ControlsController.cs
+++ solucion/Controllers/ControlsController.cs
@@ -12,6 +12,12 @@ namespace solucion.Controllers
             _context = context;
         }
 
+        public IActionResult Incom(int id)
+        {
+            var user = _context.Controls.FirstOrDefault(r => r.Id == id);
+            return Json(user);
+        }
+
 
 
 
